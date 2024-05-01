76a9dbf2c5386b4ff2dc67f902afc4c5f8b0a9d3
amanpanchal8397 <168603746+amanpanchal8397@users.noreply.github.com>
2024-05-01 05:40:30 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // export const BASE_URL = "https://hangobackend.onrender.com/api/v1";
-export const BASE = "http://localhost:4000";
+export const BASE = https://hangonbackendd.onrender.com";
 export const BASE_URL = `${BASE}/api/v1`;
